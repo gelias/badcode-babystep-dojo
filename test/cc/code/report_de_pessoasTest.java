26fe288ac0f2dc5e02b0e44a1248c4fac37a80f9
Guilherme Elias <guilherme.elias@gmail.com>
2017-01-20 11:29:06 -0200

    inicio teste

--- test/cc/code/report_de_pessoasTest.java
+++ test/cc/code/report_de_pessoasTest.java
@@ -6,4 +6,10 @@ import org.junit.Test;
 
 public class report_de_pessoasTest {
 
+	@Test
+	public void deveFazerConcatenaPessoa() {
+		report_de_pessoas report = new report_de_pessoas();
+		
+		report_de_pessoas.
+	}
 }
